4cbfeb96a2e7bcc628e987248ff05a637e127f0e
dydtmd6801 <dydtmd6801@naver.com>
2023-10-24 15:41:37 +0900

    Feat: 공원 만들기 기능 구현
    공원 정보를 보고 시작점 및 공원의 장애물 배치

--- Lv.1/src/solution03/Solution.java
+++ Lv.1/src/solution03/Solution.java
@@ -1,8 +1,22 @@
 package solution03;
 
+import java.util.Arrays;
+
 public class Solution {
     public int[] solution(String[] park, String[] routes) {
         int[] answer = {};
         return answer;
     }
+
+    public String[][] generatePark(String[] park) {
+        String[][] newPark = new String[park[0].length()][park.length];
+        for (int i = 0; i < park.length; i++) {
+            String[] obstacle = park[i].split("");
+            for (int j = 0; j < obstacle.length; j++) {
+                newPark[i][j] = obstacle[j];
+            }
+        }
+        return newPark;
+    }
+
 }
