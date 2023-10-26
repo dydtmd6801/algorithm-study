641d85c4038d49e5005e61365ab8e17d8643a250
dydtmd6801 <dydtmd6801@naver.com>
2023-10-26 15:43:34 +0900

    Test: 바탕화면 정보를 가진 문자열로 2차원 배열 만들기 테스트
    바탕화면의 정보가 문자열로 주어지는데, 그걸 2차원 배열로 만드는 테스트

--- Lv.1/test/solution04/SolutionTest.java
+++ Lv.1/test/solution04/SolutionTest.java
@@ -1,6 +1,7 @@
 package solution04;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,5 +45,14 @@ class SolutionTest {
         int[] result = {1, 0, 2, 1};
 
         assertThat(solution.solution(wallpaper)).isEqualTo(result);
+    }
+
+    @DisplayName("바탕화면 정보의 문자열로 2차원 배열 만들기")
+    @Test
+    void generateDesktopTest() {
+        String[] wallpaper = {"..", "#."};
+        String[][] desktop = {{".","."},{".","#"}};
+
+        assertThat(solution.generateDesktop(wallpaper)).isEqualTo(desktop);
     }
 }
