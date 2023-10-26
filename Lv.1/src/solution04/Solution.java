217759ad5ea74eab6e6f6c10717de5e53fd3c5ad
dydtmd6801 <dydtmd6801@naver.com>
2023-10-26 14:56:24 +0900

    Feat: 각 좌표 값 중 제일 작은 수와 큰 수 찾는 기능 구현
    각 좌표 값 중 세로부분, 가로부분에 해당하는 값 중 제일 작은 수, 큰 수를 찾는 기능

--- Lv.1/src/solution04/Solution.java
+++ Lv.1/src/solution04/Solution.java
@@ -33,4 +33,73 @@ class Solution {
         return fileCoordinates;
     }
 
+    public int[] findMinMax(List<int[]> fileCoordinates) {
+        int[] coordinate = fileCoordinates.get(0);
+        DragCoordinate drag = new DragCoordinate(coordinate[0], coordinate[0], coordinate[1], coordinate[1]);
+        for (int i = 1; i < fileCoordinates.size(); i++) {
+            coordinate = fileCoordinates.get(i);
+            int left = coordinate[0];
+            int right = coordinate[1];
+            if (drag.getLeftMin() > left) {
+                drag.setLeftMin(left);
+            }
+            if (drag.getLeftMax() < left) {
+                drag.setLeftMax(left);
+            }
+            if (drag.getRightMin() > right) {
+                drag.setRightMin(right);
+            }
+            if (drag.getRightMax() < right) {
+                drag.setRightMax(right);
+            }
+        }
+        return new int[]{drag.getLeftMin(), drag.getRightMin(), drag.getLeftMax() + 1, drag.getRightMax() + 1};
+    }
+}
+
+class DragCoordinate {
+
+    private int leftMin;
+    private int leftMax;
+    private int rightMin;
+    private int rightMax;
+
+    public DragCoordinate(int leftMin, int leftMax, int rightMin, int rightMax) {
+        this.leftMin = leftMin;
+        this.leftMax = leftMax;
+        this.rightMin = rightMin;
+        this.rightMax = rightMax;
+    }
+
+    public int getLeftMin() {
+        return leftMin;
+    }
+
+    public void setLeftMin(int leftMin) {
+        this.leftMin = leftMin;
+    }
+
+    public int getLeftMax() {
+        return leftMax;
+    }
+
+    public void setLeftMax(int leftMax) {
+        this.leftMax = leftMax;
+    }
+
+    public int getRightMin() {
+        return rightMin;
+    }
+
+    public void setRightMin(int rightMin) {
+        this.rightMin = rightMin;
+    }
+
+    public int getRightMax() {
+        return rightMax;
+    }
+
+    public void setRightMax(int rightMax) {
+        this.rightMax = rightMax;
+    }
 }
