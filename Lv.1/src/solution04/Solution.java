7ad06364b464dc62e2059fd8691e31d77a8978eb
dydtmd6801 <dydtmd6801@naver.com>
2023-10-26 11:58:17 +0900

    Feat: 바탕화면 정보를 바탕으로 2차원 배열 생성
    바탕화면에 문자열로 저장된 정보를 split 메소드로 분할 후 2차원 배열로 생성

--- Lv.1/src/solution04/Solution.java
+++ Lv.1/src/solution04/Solution.java
@@ -5,4 +5,15 @@ class Solution {
         int[] answer = {};
         return answer;
     }
+
+    public String[][] generateDesktop(String[] wallpaper) {
+        String[][] desktop = new String[wallpaper.length][wallpaper[0].length()];
+        for (int i = 0; i < wallpaper.length; i++) {
+            String[] row = wallpaper[i].split("");
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                desktop[i][j] = row[i];
+            }
+        }
+        return desktop;
+    }
 }
