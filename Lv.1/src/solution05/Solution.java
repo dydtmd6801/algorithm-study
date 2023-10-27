35ebf35cab621ab3947dc0ae4e97893b68891541
dydtmd6801 <dydtmd6801@naver.com>
2023-10-27 16:11:33 +0900

    Feat: n 미터의 벽 만들기 기능 구현
    벽의 길이와 칠 해야하는 부분의 정보를 가지고 map 자료형으로 벽 만들기

--- Lv.1/src/solution05/Solution.java
+++ Lv.1/src/solution05/Solution.java
@@ -1,8 +1,24 @@
 package solution05;
 
+import java.util.HashMap;
+
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
         return answer;
+    }
+
+    public HashMap generateWall(int n, int[] section) {
+        HashMap wall = new HashMap();
+        int unpainted = 0;
+        for (int i = 1; i <= n; i++) {
+            if (section[unpainted] == i) {
+                wall.put(i, "X");
+                unpainted++;
+                continue;
+            }
+            wall.put(i, "O");
+        }
+        return wall;
     }
 }
